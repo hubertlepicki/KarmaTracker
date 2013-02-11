1acf5d3c41a110f6d083f42ce810292dbd3e4969
Hubert Lepicki <hubert.lepicki@amberbit.com>
2013-02-11 21:37:30 +0100

    Initial Rails scaffold generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarmaTracker::Application.config.secret_key_base = 'c516f8f91db7d1bdfc9acac19f42bd9f42380e352d01556d29ed46843efc1b2c9df42812434f259bc708988fbddffdcf4ff131919530c23545b0b306a273ec54'
